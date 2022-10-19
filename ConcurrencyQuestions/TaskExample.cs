c5195a04dec0c905e078c4e6f55319cd7cb1847c
xCKtmdl <aarongab@gmail.com>
2022-10-19 05:36:25 -0400

    add the Task example code.

---
+++ ConcurrencyQuestions/TaskExample.cs
@@ -0,0 +1,43 @@
+﻿var myTask = new MyClass();
+
+Task.Run(() => myTask.MyAsync(1, ConsoleColor.Red));
+Task.Run(() => myTask.MyAsync(2, ConsoleColor.Green));
+Task.Run(() => myTask.MyAsync(3, ConsoleColor.Blue));
+
+
+
+public class MyClass
+{
+    
+    public async Task MyAsync(int threadNumber, ConsoleColor color)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            Print(threadNumber, color);
+        }
+    }
+
+
+    public static void Print(int threadNumber, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        string message = String.Empty;
+        switch (color)
+        {
+            case ConsoleColor.Red:
+                message = String.Format("thread: {0}, color: red", threadNumber);
+                break;
+
+            case ConsoleColor.Green:
+                message = String.Format("thread: {0}, color: green", threadNumber);
+                break;
+            case ConsoleColor.Blue:
+                message = String.Format("thread: {0}, color: blue", threadNumber);
+                break;
+
+            default:
+                break;
+        }
+        Console.WriteLine(message);
+    }
+}
